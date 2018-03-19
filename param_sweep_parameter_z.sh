0cbf2fb2c12f6779295e1b321527682a365d4e02
Gregory Way <gregory.way@gmail.com>
2018-03-19 11:25:45 -0700

    add script that will initiate the sweep with parameter z

---
+++ param_sweep_parameter_z.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# The purpose of this script is to determine a reasonable set of hyperparameters
+# for different input z dimensionality. The ultimate goal is to perform an
+# analysis on the latent space dimensionality, and we want to be sure that
+# the comparisons being made are not a result of poor input hyperparameters
+
+# Note this was run at the PMACS cluster at University of Pennsvylvania on a
+# cluster of 8 NVIDIA GEFORCE GTX Ti GPUs.
+
+PARAM_FILE_PREFIX='config/z_parameter_sweep_'
+PMACS_FILE='config/pmacs_config.tsv'
+PYTHON_PATH='~/.conda/envs/tybalt-gpu/bin/python'
+ALGORITHMS=( 'tybalt', 'adage' )
+
+for alg in "${ALGORITHMS[@]}"
+do
+    PARAM_FILE=$PARAM_FILE_PREFIX$alg'.tsv'
+    ~/.conda/envs/tybalt-gpu/bin/python scripts/num_components_paramsweep.py \
+          --parameter_file $PARAM_FILE \
+          --config_file $PMACS_FILE \
+          --algorithm $alg \
+          --python_path $PYTHON_PATH \
+          --param_folder 'z_param_sweep_'$alg
+done
