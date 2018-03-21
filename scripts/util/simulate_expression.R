fa194fb3baba2cd5ec856b627c927ab97ad57456
gwaygenomics <gregory.way@gmail.com>
2018-03-21 12:30:37 -0400

    generate 5 modules instead of 3
    also add more description on what is being simulated. Add figures too.

--- scripts/util/simulate_expression.R
+++ scripts/util/simulate_expression.R
@@ -18,7 +18,7 @@ set.seed(1234)
 simulateExpression <- function(n, num_sets, num_genes, num_modules,
                                background_noise, min_cor, max_cor,
                                sample_set_A, sample_set_B, mod_prop,
-                               leave_out_A, leave_out_B) {
+                               leave_out_A, leave_out_B, return_eigen=FALSE) {
   # Output a simulated gene expression matrix using WGCNA
   #
   # Arguments:
@@ -46,7 +46,7 @@ simulateExpression <- function(n, num_sets, num_genes, num_modules,
     matrix(rnorm(num_modules * n / 2, mean = sample_set_B, sd = 1),
            n / 2, num_modules, byrow = TRUE)
   )
-
+  
   # Simulate Expression
   # the two matrices differ based on the leave out argument. This is to enable
   # an assessment of latent space arithmetic to test if an algorithm can isolate
@@ -85,6 +85,11 @@ simulateExpression <- function(n, num_sets, num_genes, num_modules,
   colnames(x_matrix) <- gsub('[.]', '_', colnames(x_matrix))
   x_matrix$groups <- sample_labels
   x_matrix <- rbind(gene_labels, x_matrix)
+  
+  if (return_eigen) {
+    return(list(x_matrix, eigen_gene_samples))
+  }
+  
   return(x_matrix)
 }
 
@@ -100,8 +105,8 @@ num_modules <- 5
 min_cor <- 0.4
 max_cor <- 0.9
 
-sample_set_A <- c(1, -1, -3)
-sample_set_B <- c(-2, 4, 2)
+sample_set_A <- c(1, -1, 3, 0, -3)
+sample_set_B <- c(0, 6, 3, -3, 1)
 
 mod_prop <- c(0.25, 0.2, 0.15, 0.1, 0.05, 0.25)
 
@@ -132,3 +137,72 @@ for (n in ns) {
     }
   }
 }
+
+# Plot an example of simulated data
+example_sim <- simulateExpression(n = 500,
+                                  num_genes = 1000,
+                                  background_noise = 0.1,
+                                  num_sets = num_sets,
+                                  num_modules = num_modules,
+                                  min_cor = min_cor,
+                                  max_cor = max_cor,
+                                  sample_set_A = sample_set_A,
+                                  sample_set_B = sample_set_B,
+                                  mod_prop = mod_prop,
+                                  leave_out_A = leave_out_A,
+                                  leave_out_B = leave_out_B,
+                                  return_eigen = TRUE)
+
+# The eigen samples matrix forms the basis of the simulated patterns. It
+# contains both sample group and gene module information
+eigen_samples <- example_sim[[2]] 
+
+# Plot the module correlations (there are 5 modules) Module 3 is made
+# uncoupled from other modules, since it will be the focus of the latent space
+# arithmetic test. Some modules are correlated to mimic real data.
+eigen_module_file <- file.path("figures", "example_eigen_module_plot.png")
+module_corr <- ggcorrplot::ggcorrplot(cor(eigen_samples), hc.order = FALSE,
+                            outline.color = 'white') +
+  ggtitle("Correlation across artificial gene modules \n(n = 5)")
+ggsave(eigen_module_file, module_corr, height = 5, width = 6)
+
+# Plot the sample correlations for this example. There are 1000 simulated
+# samples. Each block of samples are distinct units - but it is important
+# to note that this is before adding noise and dropping out module 3.
+eigen_sample_file <- file.path("figures", "example_eigen_sample_plot.png")
+sample_corr <- ggcorrplot::ggcorrplot(cor(t(eigen_samples)), hc.order = FALSE,
+                                      outline.color = 'white') +
+  ggtitle("Correlation across artificial sample groups\n(n = 500)")
+ggsave(eigen_sample_file, sample_corr, height = 5, width = 6)
+
+# Now, extract the actual sampled data and visualize a sample by gene heatmap.
+# The red and orange samples are sampled from the same sample set module origin
+# as are the blue and green. The green and orange samples have random noise
+# expression of module 3.
+simulated_example_file <- file.path("figures", "example_simulated_data.pdf")
+sampled_data <- example_sim[[1]][2:nrow(example_sim[[1]]),
+                                 1:ncol(example_sim[[1]]) - 1]
+pdf(simulated_example_file)
+heatmap.2(as.matrix(sampled_data), trace = "none",
+          RowSideColors =  c(rep('blue', 250),
+                            rep('red', 250),
+                            rep('green', 250),
+                            rep('orange', 250)),
+          labRow = "", labCol = "", scale = 'none',
+          hclustfun = function(x) hclust(x, method = 'average'),
+          distfun = function(x) dist(x, method = 'euclidean'),
+          dendrogram = "none", Rowv = TRUE, Colv = FALSE)
+dev.off()
+
+simulated_example_png <- file.path("figures", "example_simulated_data.png")
+png(simulated_example_png)
+heatmap.2(as.matrix(sampled_data), trace = "none",
+          RowSideColors =  c(rep('blue', 250),
+                             rep('red', 250),
+                             rep('green', 250),
+                             rep('orange', 250)),
+          labRow = "", labCol = "", scale = 'none',
+          hclustfun = function(x) hclust(x, method = 'average'),
+          distfun = function(x) dist(x, method = 'euclidean'),
+          dendrogram = "none", Rowv = TRUE, Colv = FALSE)
+dev.off()
