631bc19748d41253e2092416a9114bebe7bb38b9
gwaygenomics <gregory.way@gmail.com>
2018-04-06 11:03:55 -0400

    correct typo

--- tybalt/models.py
+++ tybalt/models.py
@@ -347,7 +347,7 @@ class Adage(BaseModel):
         self.decoder = Model(encoded_input, decoder_layer(encoded_input))
 
         if self.tied_weights:
-            # The keras graph is build differently for a tied weight model
+            # The keras graph is built differently for a tied weight model
             # Build a model with input and output Tensors of the encoded layer
             self.encoder = Model(self.encoded.input, self.encoded.output)
         else:
